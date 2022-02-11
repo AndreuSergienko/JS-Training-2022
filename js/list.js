22280836c83ffec106c4e7e9a242f6d5f52b203f
AndreySerg1enko <kolsergienko@gmail.com>
2022-02-12 00:29:16 +0300

    TASK1

---
+++ js/list.js
@@ -0,0 +1,25 @@
+let li = document.querySelectorAll("li");
+let links = document.querySelectorAll("a");
+let ul = document.querySelector("ul");
+ul.style = "padding:0";
+li.forEach((l) => {
+	l.style = "list-style:none";
+});
+links.forEach((link) => {
+	link.style = "text-decoration:none;font-size:25px;color:#e86";
+	link.addEventListener("mouseover", function (e) {
+		e.preventDefault();
+		links.forEach((item) => {
+			item.style = "text-decoration:none;font-size:25px;color:#e86";
+		});
+		link.style =
+			"text-decoration:none;font-size:25px;color:rgba(0, 0, 0, 1);transition:all 0.3s ease-out";
+	});
+	link.addEventListener("mouseleave", function (e) {
+		links.forEach((item) => {
+			e.preventDefault();
+			item.style =
+				"text-decoration:none;font-size:25px;color:#e86;transition:all 0.2s ease-out";
+		});
+	});
+});

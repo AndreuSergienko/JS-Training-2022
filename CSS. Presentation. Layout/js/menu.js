2d6096cf4052babfc648ca48b5463afb112218f8
AndreySerg1enko <kolsergienko@gmail.com>
2022-03-10 20:02:08 +0300

    TASK3

--- CSS. Presentation. Layout/js/menu.js
+++ CSS. Presentation. Layout/js/menu.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
     const menu = document.querySelector('.menu')
     window.addEventListener('scroll', function (e) {
         let windowScroll = window.pageYOffset;
-        console.log(windowScroll);
-        if (windowScroll > 35) {
+        if (windowScroll > 30) {
             menu.classList.add('scrolled')
         } else {
             menu.classList.remove('scrolled')

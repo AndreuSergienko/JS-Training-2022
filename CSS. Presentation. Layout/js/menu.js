7febbfd67e2bb9b31de5834bd107774feaf46ecb
Andrey <kolsergienko@gmail.com>
2022-03-14 15:05:37 +0300

    TASK3

--- CSS. Presentation. Layout/js/menu.js
+++ CSS. Presentation. Layout/js/menu.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function (e) {
-    const menu = document.querySelector('.menu')
-    window.addEventListener('scroll', function (e) {
-        let windowScroll = window.pageYOffset;
-        if (windowScroll > 30) {
-            menu.classList.add('scrolled')
-        } else {
-            menu.classList.remove('scrolled')
-        }
-    })
-})
+document.addEventListener("DOMContentLoaded", function (e) {
+  const menu = document.querySelector(".menu");
+  window.addEventListener("scroll", function (e) {
+    let windowScroll = window.pageYOffset;
+    if (windowScroll > 30) {
+      menu.classList.add("scrolled");
+    } else {
+      menu.classList.remove("scrolled");
+    }
+  });
+});

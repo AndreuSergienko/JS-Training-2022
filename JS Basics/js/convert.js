fbaa47a08bb75d3a2fd4d5cc788afe9b6e009cd5
Andrey <kolsergienko@gmail.com>
2022-03-21 23:49:40 +0300

    TASK5

--- JS Basics/js/convert.js
+++ JS Basics/js/convert.js
@@ -1,21 +1,28 @@
-let input = document.querySelector('.converter-input')
-let labels = document.querySelectorAll('.converter-label')
-let spans = document.querySelectorAll('.output')
-let converterField = document.querySelector('.converter')
+// let input = document.querySelector('.converter-input')
+// let labels = document.querySelectorAll('.converter-label')
+// let spans = document.querySelectorAll('.output')
+// let converterField = document.querySelector('.converter')
 
-const RATES = [
-    USD = 3.25,
-    EUR = 3.6,
-    RUB = 3.14,
-]
+// const RATES = [
+//     USD = 3.25,
+//     EUR = 3.6,
+//     RUB = 3.14,
+// ]
 
-input.oninput = function () {
-    for (let i = 0; i < RATES.length; i++) {
-        let output = document.querySelector('#output' + [i]);
-        output.innerHTML = RATES[i] * input.value;
-    }
+// input.oninput = function () {
+//     for (let i = 0; i < RATES.length; i++) {
+//         let output = document.querySelector('#output' + [i]);
+//         output.innerHTML = RATES[i] * input.value;
+//     }
+// }
+
+
+const obj = {
+    a: 1,
+    b: 2
 }
 
+const { a } = obj;
 
 
 

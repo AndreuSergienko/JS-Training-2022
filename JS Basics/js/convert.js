69ac556ae7a118ccee90031b8d7cdaccb424f8de
Andrey <kolsergienko@gmail.com>
2022-03-21 22:39:08 +0300

    TASK5

---
+++ JS Basics/js/convert.js
@@ -0,0 +1,23 @@
+let input = document.querySelector('.converter-input')
+let labels = document.querySelectorAll('.converter-label')
+let spans = document.querySelectorAll('.output')
+let converterField = document.querySelector('.converter')
+
+const RATES = [
+    USD = 3.25,
+    EUR = 3.6,
+    RUB = 3.14,
+]
+
+input.oninput = function () {
+    for (let i = 0; i < RATES.length; i++) {
+        let output = document.querySelector('#output' + [i]);
+        output.innerHTML = RATES[i] * input.value;
+    }
+}
+
+
+
+
+
+

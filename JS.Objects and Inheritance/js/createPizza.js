b2026725b2165b10c41e302560dd46f17ee23804
Andrey <kolsergienko@gmail.com>
2022-04-28 01:50:06 +0300

    TASK6

---
+++ JS.Objects and Inheritance/js/createPizza.js
@@ -0,0 +1,74 @@
+const ingredientsItems = document.querySelectorAll('.ingredients__item');
+const createBtn = document.getElementById('create-button');
+const doughSelect = document.getElementById('dough-select');
+let ingredientNamesArray = [];
+let ingredientCaloriesArray = [];
+let ingredientPriceArray = [];
+
+class Pizza {
+    constructor(ingredients, calories, price) {
+        this.ingredients = ingredients;
+        this.calories = calories + 'kkal';
+        this.price = price + '$';
+    }
+
+}
+
+ingredientsItems.forEach(item => {
+    item.addEventListener('click', e => {
+        const clickedItem = e.target.closest('.ingredients__item');
+        if (!clickedItem.classList.contains('checked')) {
+            const ingredientName = clickedItem.children[1].children[0].innerHTML;
+            const ingredientCal = clickedItem.children[1].children[1].innerHTML;
+            const ingredientPrice = clickedItem.children[1].children[2].innerHTML;
+            ingredientNamesArray.push(ingredientName);
+            if (ingredientCaloriesArray.length > 0 && ingredientPriceArray.length > 0) {
+                ingredientCaloriesArray.push(+ingredientCal);
+                ingredientPriceArray.push(+ingredientPrice);
+                let sumOfCalories = ingredientCaloriesArray.reduce((acc, num) => {
+                    return acc + num;
+                });
+                let sumOfPrice = ingredientPriceArray.reduce((acc, num) => {
+                    return acc + num;
+                });
+                let readyPizza = new Pizza(ingredientNamesArray, sumOfCalories, sumOfPrice);
+                doughSelect.onselect = this.value;
+                doughSelect.onchange = this.value;
+                createBtn.addEventListener('click', () => {
+                    gatherData(readyPizza, doughSelect.value);
+                });
+            } else {
+                ingredientCaloriesArray.push(+ingredientCal);
+                ingredientPriceArray.push(+ingredientPrice);
+                let sumOfCalories = ingredientCaloriesArray.reduce((acc, num) => {
+                    return acc + num;
+                });
+                let sumOfPrice = ingredientPriceArray.reduce((acc, num) => {
+                    return acc + num;
+                });
+                let readyPizza = new Pizza(ingredientNamesArray, sumOfCalories, sumOfPrice);
+                doughSelect.onselect = this.value;
+                doughSelect.onchange = this.value;
+                createBtn.addEventListener('click', () => {
+                    gatherData(readyPizza, doughSelect.value);
+                });
+            }
+        };
+    });
+});
+
+
+function gatherData(pizza, dough) {
+
+
+    resultWindow.innerHTML =
+        `
+        <h2>Поздравляю вы сделали свою пиццу:</h2>
+        <div>Тесто: <span>${dough}</span></div>
+        <div>Начинка: <span>${pizza.ingredients}</span></div>
+        <div>Калории: <span>${pizza.calories}</span></div>
+        <div>С вас: <span>${pizza.price}</span></div>
+    `
+    resultWindow.classList.add('show');
+}
+
--- JS.Objects and Inheritance/js/main.js
+++ JS.Objects and Inheritance/js/main.js
@@ -1,11 +1,35 @@
 const ingredients = document.getElementById('ingredients');
+const addBtn = document.querySelectorAll('.pizza__add-btn');
+const pizzaDescription = document.querySelectorAll('.pizza__description');
+const resultWindow = document.getElementById('result-window');
+
+for (const btn of addBtn) {
+    btn.addEventListener('click', (e) => {
+        ingredientNamesArray = [];
+        ingredientCaloriesArray = [];
+        ingredientPriceArray = [];
+        const currBtn = e.target.dataset.add;
+        pizzaDescription.forEach(item => {
+            const pizzaDescriptionData = item.dataset.description;
+            if (currBtn == pizzaDescriptionData) {
+                const pizzaDescriptionContent = item.innerHTML;
+                resultWindow.innerHTML = `
+                    <h2>Поздравляю вы заказали пиццу:</h2>
+                    ${pizzaDescriptionContent}
+                    <button type="button" class="order-button">Оформить заказ</button>
+                `;
+            }
+        });
+        resultWindow.classList.add('show');
+    })
+}
 
 ingredients.addEventListener('click', e => {
     const ingredientItem = e.target.closest('.ingredients__item');
     ingredientItem.classList.toggle('checked');
     if (ingredientItem.classList.contains('checked')) {
         ingredientItem.children[0].checked = true;
-    }else {
+    } else {
         ingredientItem.children[0].checked = false;
     }
 })
